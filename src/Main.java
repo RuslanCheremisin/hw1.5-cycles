f6c91cc9c073fb2036c9d26933c60fd7b42fbc7d
rus <ruslan.cheremisin@gmail.com>
2022-09-05 23:40:36 +1000

    ДЗ по циклам №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+    public static void task1(){
+
+        for (int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+        System.out.println("\n");
+    }
+    public static void task2(){
+        for (int i=10; i>=1; i--){
+            System.out.println(i);
+        }
+        System.out.println("\n");
+    }
+    public static void task3(){
+        for (int i=0; i<=17; i=i+2){
+            System.out.println(i);
+        }
+        System.out.println("\n");
+    }
+    public static void task4(){
+        for (int i=10; i>=-10; i--){
+            System.out.println(i);
+        }
+        System.out.println("\n");
     }
 }
